ad85cb2bb6f23f7b3c3ae49ad88bdadd4632c4cc
Liu Yang <ly_0913@foxmail.com>
2021-04-26 17:11:35 +0800

    update

---
+++ 04-SequentialLogic/125-Fsm3.v
@@ -0,0 +1,13 @@
+module top_module(
+    input clk,
+    input in,
+    input areset,
+    output out); //
+
+    // State transition logic
+
+    // State flip-flops with asynchronous reset
+
+    // Output logic
+
+endmodule
