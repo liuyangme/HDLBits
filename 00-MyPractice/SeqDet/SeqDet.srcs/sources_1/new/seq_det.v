c00c7b6400f09f54452949191654d23f324ea586
Liu Yang <ly_0913@foxmail.com>
2021-07-27 19:27:18 +0800

    update

---
+++ 00-MyPractice/SeqDet/SeqDet.srcs/sources_1/new/seq_det.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/07/26 10:43:26
+// Design Name: 
+// Module Name: seq_det
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module seq_det(
+    input x,
+    input clk_i,
+    input rst_n,
+    output z
+    );
+
+    reg []
+
+
+
+
+
+endmodule
